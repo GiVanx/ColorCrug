b3e1aa796f103233cbca8efcb64abda42dcf3566
Gandacov Ivan <gandacov96@gmail.com>
2019-03-24 16:01:51 +0100

    Create a moving pad (left-right) at the bottom of the screen

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+    public int speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(leftKey))
+        {
+            Vector3 tempVect = new Vector3(1, 0, 0);
+            tempVect = tempVect.normalized * (-speed) * Time.deltaTime;
+            rigidBody.MovePosition(transform.position + tempVect);
+            
+        } else if (Input.GetKey(rightKey))
+        {
+            Vector3 tempVect = new Vector3(1, 0, 0);
+            tempVect = tempVect.normalized * speed * Time.deltaTime;
+            rigidBody.MovePosition(transform.position + tempVect);
+
+        } else
+        {
+            rigidBody.velocity.Set(0, 0);
+        }
+    }
+}
